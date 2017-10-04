9127b6ec393e6f41deebcdd9dfd0e40e96fe102f
João Vitor <jvsantillo@gmail.com>
2017-10-04 13:35:17 -0300

    Update index

--- app3/index.android.js
+++ app3/index.android.js
@@ -3,37 +3,70 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 
-class ComponentePersonalizado extends Component {
-  render() {
-    return(
+const Estilo = {
+  
+  principal: {
+    backgroundColor: '#D4D2D5',
+    flex: 1, 
+    alignItems: 'center',
+    justifyContent: 'center'
+  }, 
+  
+  estiloBotao: {
+    
+    backgroundColor: '#342E37',
+    borderColor: '#FAFFFD',
+    borderWidth: 2,
+    borderRadius: 5,
+    shadowColor: '#fff',
+    shadowOffset: {width:2, height:0},
+    shadowOpacity: 0.4, 
+    marginTop: 20,
+    marginHorizontal: 10,
+    flexDirection: 'row',
+    paddingVertical: 10, 
+    paddingHorizontal: 30
+    
+  }
+}
 
+class ComponentePersonalizado extends Component {
+  
+  render() {
+    return(      
       <View>
-        <Text>Primeiro</Text>
-        <Text>Segundo</Text>
         <Text>{this.props.prop1}</Text>
-        <Text>{this.props.prop2}</Text>
-        <Text>{this.props.prop3}</Text>
-        <TouchableOpacity>
-          
-        </TouchableOpacity>
       </View> 
 
     );
   }
 }
 
+const botaoApertado =  () =>  {
+   alert("O botão foi apertado");
+}
+
+
 class app3 extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { texto: "Texto padrão"}
+  }
+
   render() {
    return(
-    
-    <ComponentePersonalizado prop1 = "bla" prop2 = "ble" prop3 = "bleu"></ComponentePersonalizado>
+    <View style = {Estilo.principal}>
+        <Button onPress = {botaoApertado} label = "Toque para mudar o texto"></Button>
+        <ComponentePersonalizado prop1 = {this.state.texto} ></ComponentePersonalizado>
+    </View>
 
    );
   }
 }
 
-AppRegistry.registerComponent('ComponentePersonalizado', () => ComponentePersonalizado);
 AppRegistry.registerComponent('app3', () => app3);
